fe617431132737f4c980524e0fff4e3d416f1d16
bikboktech <bikboktech@gmail.com>
2023-06-08 12:00:10 +0200

    feat: finishes frontend for Spotify Playlist downloader

--- src/app/musicology/PlaylistDownloader.tsx
+++ src/app/musicology/PlaylistDownloader.tsx
@@ -1,64 +1,112 @@
-import React from "react";
-import {
-  Box,
-  Typography,
-  FormGroup,
-  FormControlLabel,
-  Button,
-  Stack,
-  Checkbox,
-} from "@mui/material";
-import Link from "next/link";
+import React, { useState } from "react";
+import { Box, Typography, Button, LinearProgress } from "@mui/material";
+import { Controller, FieldValues, useForm } from "react-hook-form";
 
 import CustomTextField from "@/app/(DashboardLayout)/components/forms/theme-elements/CustomTextField";
 
-interface loginType {
+interface downloaderType {
   title?: string;
-  subtitle?: JSX.Element | JSX.Element[];
   subtext?: JSX.Element | JSX.Element[];
 }
 
-const PlaylistDownloader = ({ title, subtitle, subtext }: loginType) => (
-  <>
-    {title ? (
-      <Typography fontWeight="700" variant="h2" mb={1}>
-        {title}
-      </Typography>
-    ) : null}
+const PlaylistDownloader = ({ title, subtext }: downloaderType) => {
+  const [loading, setLoading] = useState(false);
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-    {subtext}
+  const onSubmit = async (data: FieldValues) => {
+    setLoading(true);
+    try {
+      const response = (await fetch(
+        `${process.env.NEXT_PUBLIC_API_BASE_URL}/playlist-management/download`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ playlistLink: data.spotifyPlaylistLink }),
+        }
+      )) as Response;
 
-    <Box my={2}>
-      {/* <Typography
-        variant="subtitle1"
-        fontWeight={600}
-        component="label"
-        htmlFor="spotifyPlaylistLink"
-        mb="5px"
-      >
-        Spotify Playlist Link
-      </Typography> */}
-      <CustomTextField
-        variant="outlined"
-        label="Spotify Playlist Link"
-        placeholder="ex. https://open.spotify.com/playlist/0S3oyFxQ3iglOGVxFTF79K?si=ea7e596097014917"
-        fullWidth
-      />
-    </Box>
-    <Box>
-      <Button
-        color="secondary"
-        variant="contained"
-        size="large"
-        fullWidth
-        component={Link}
-        href="/"
-        type="submit"
-      >
-        Download
-      </Button>
-    </Box>
-  </>
-);
+      const contentDisposition = response.headers.get("Content-Disposition");
+
+      const filenameMatch =
+        contentDisposition && contentDisposition.match(/filename="(.+)"/);
+      const filename = filenameMatch ? filenameMatch[1] : "playlist.zip";
+
+      const blob = await response.blob();
+
+      const downloadUrl = window.URL.createObjectURL(blob);
+
+      const link = document.createElement("a") as HTMLAnchorElement;
+      link.href = downloadUrl;
+      link.setAttribute("download", filename);
+
+      document.body.appendChild(link);
+
+      link.click();
+
+      link.parentNode?.removeChild(link);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.error("Error downloading playlist:", error);
+    }
+  };
+
+  return (
+    <>
+      {title ? (
+        <Typography fontWeight="700" variant="h2" mb={1}>
+          {title}
+        </Typography>
+      ) : null}
+
+      {subtext}
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Box my={2}>
+          <Controller
+            name="spotifyPlaylistLink"
+            control={control}
+            rules={{ required: true }}
+            render={({ field }) => (
+              <CustomTextField
+                {...field}
+                variant="outlined"
+                label="Spotify Playlist Link"
+                placeholder="ex. https://open.spotify.com/playlist/0S3oyFxQ3iglOGVxFTF79K?si=ea7e596097014917"
+                fullWidth
+              />
+            )}
+          />
+
+          {errors.spotifyPlaylistLink && <span>This field is required</span>}
+        </Box>
+        <Box>
+          {loading ? (
+            <LinearProgress
+              sx={{
+                backgroundColor: "transparent",
+              }}
+            />
+          ) : (
+            <Button
+              color="secondary"
+              variant="contained"
+              size="large"
+              fullWidth
+              type="submit"
+            >
+              Download
+            </Button>
+          )}
+        </Box>
+      </form>
+    </>
+  );
+};
 
 export default PlaylistDownloader;
